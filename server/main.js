00f5f54e622a573e1159e77531cf12cb02fb1f02
Helio Alves <helios25@gmail.com>
2017-01-09 14:46:44 -0500

    create items db and created imports structure and create main App component with Header component

--- server/main.js
+++ server/main.js
@@ -1,5 +1,18 @@
 import { Meteor } from 'meteor/meteor';
 
+import Items from '../imports/api/Items';
+
+
 Meteor.startup(() => {
   // code to run on server at startup
+  Items.insert({
+    itemOne: {
+      text: 'Hi',
+      value: 0
+    },
+    itemTwo: {
+      text: 'Hello',
+      value: 0
+    }
+  });
 });
